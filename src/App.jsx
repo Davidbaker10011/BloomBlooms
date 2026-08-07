436f20a8649bf203406662c0688df2bc380edbfb
Davidbaker10011 <davidbaker10011@gmail.com>
2026-08-07 11:31:28 -0400

    Add components to App.jsx for layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,25 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Resources from "./components/Resources";
+import KnowYourRights from "./components/KnowYourRights";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main id="home">
+        <Hero />
+        <Resources />
+        <KnowYourRights />
+        <About />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
