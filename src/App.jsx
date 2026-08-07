9d22672c4f826d0a1ad26badbc87555c2deb7a6b
Davidbaker10011 <davidbaker10011@gmail.com>
2026-08-07 11:50:02 -0400

    Refactor App component structure and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import Header from "./components/Header";
+import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
-import Resources from "./components/Resources";
-import KnowYourRights from "./components/KnowYourRights";
+import SearchBar from "./components/SearchBar";
 import About from "./components/About";
+import KnowYourRights from "./components/KnowYourRights";
+import Resources from "./components/Resources";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
-
-      <main id="home">
-        <Hero />
-        <Resources />
-        <KnowYourRights />
-        <About />
-      </main>
-
+      <Navigation />
+      <Hero />
+      <SearchBar />
+      <About />
+      <KnowYourRights />
+      <Resources />
       <Footer />
     </>
   );
