eace131f4754ef343ff63827231806489bff7a78
Davidbaker10011 <davidbaker10011@gmail.com>
2026-08-07 11:45:24 -0400

    Add Navigation component with links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,1 +1,12 @@
+function Navigation() {
+  return (
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#rights">Know Your Rights</a>
+      <a href="#resources">Resources</a>
+      <a href="#about">About</a>
+    </nav>
+  );
+}
 
+export default Navigation;
