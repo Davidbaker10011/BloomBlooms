01f4b88a0315f173631c46916139784166fe2bea
Davidbaker10011 <davidbaker10011@gmail.com>
2026-08-07 11:44:56 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Justice Access America</h1>
+      <p>Know Your Rights. Find Resources. Get Help.</p>
+    </header>
+  );
+}
 
+export default Header;
